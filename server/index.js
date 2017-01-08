4b4266d52d676c565670bc1fc5753620c226dc57
Joost Kiens <me@joostkiens.com>
2017-01-08 18:56:49 +0100

    remove unused python files

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const { port, host } = config.server
 const isProduction = process.env.NODE_ENV === 'production'
+let pins = []
 
 app.use(historyApiFallback())
 app.use(bodyParser.json())
@@ -14,12 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 if (isProduction) {
   const five = require('johnny-five')
-  const chipio = require('chipio')
-
+  const chipio = require('chip-io')
   const board = new five.Board({
     io: new chipio()
   })
-  const pins = config.lights.map(l => new five.Pin(l.pinOut, 1))
+
+  ipns = config.lights.map(l => new five.Pin(l.pinOut, 1))
 
   board.on('ready', function() {
     // will serve a static directory
@@ -34,12 +35,12 @@ if (isProduction) {
 app.use('/static', express.static(path.resolve(__dirname, '..', config.build.static)))
 
 app.post('/switch', function (req, res) {
-  if (!isProduction) return
+  if (isProduction) {
+    req.body.state
+    ? pins[req.body.index].high()
+    : pins[req.body.index].low()
+  }
   res.send('Got a POST request')
-
-  req.body.state
-  ? pins[res.body.index].high()
-  : pins[res.body.index].low()
 })
 
 app.listen(port, host)
@@ -48,12 +49,4 @@ function developmentMiddleware (port, host) {
   const webpackConfig = require('../build/webpack.config')
   const webpackMiddleware = require('./middleware/webpackMiddleware')
   return webpackMiddleware(webpackConfig, Object.assign({}, config.webpackDevServer, port, host))
-}
-
-function sendToPin (state, index) {
-  //sendJsonToPy({ state, index })
-}
-
-function sendJsonToPy (json) {
-  //child.stdin.write(JSON.stringify(json))
 }
